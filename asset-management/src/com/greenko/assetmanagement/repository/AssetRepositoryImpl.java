94888c2ace7e98e003aa89c4413efebe012dac66
ramanujds <ramanujds9@gmail.com>
2026-01-21 15:53:41 +0530

    Asset repository implementation

--- asset-management/src/com/greenko/assetmanagement/repository/AssetRepositoryImpl.java
+++ asset-management/src/com/greenko/assetmanagement/repository/AssetRepositoryImpl.java
@@ -1,7 +1,34 @@
 package com.greenko.assetmanagement.repository;
 
+import com.greenko.assetmanagement.model.Asset;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class AssetRepositoryImpl implements AssetRepository{
 
+    List<Asset> assets = new ArrayList<>();
 
+    public Asset addAsset(Asset asset) {
+        assets.add(asset);
+        return asset;
+    }
 
+    public Asset findByName(String name) {
+        for (var asset:assets){
+            if(asset.getName().equalsIgnoreCase(name)){
+                return asset;
+            }
+        }
+        return null;
+    }
+
+    public List<Asset> findAllAssets() {
+        return assets;
+    }
+
+    public void deleteAsset(String name) {
+        var asset = findByName(name);
+        assets.remove(asset);
+    }
 }
